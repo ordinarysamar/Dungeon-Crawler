6124d2744bb880432c5689cda70ce4bac884c219
smr.me <smr.20samar@gmail.com>
2021-06-06 19:17:10 +0530

    Updated the player movement code...
    - Modified the player code for smooth movement.
    - Modified the World scene.

--- source/scripts/Player.gd
+++ source/scripts/Player.gd
@@ -1,31 +1,35 @@
+extends KinematicBody2D
 
-extends Actor
+export var ACCELERATION = 500
+export var MAX_SPEED = 105
+export var FRICTION = 500
+export var GRAVITY = 700
+export var JUMP_HEIGHT = 500
 
-# const ACCELERATION 
-# const FRICTION
-# const MAX_SPEED 
-
-func _physics_process(_delta):
-	var jump_interepted = Input.is_action_just_released("ui_up") and velocity.y < 0.0
-	var input_velocity = direction()
-	velocity = calculate_velocity(velocity , input_velocity , speed , jump_interepted)
-	velocity = move_and_slide(velocity , FLOOR_NORMAL)
+var velocity = Vector2.ZERO	
 
 
-func direction() -> Vector2:
-	return Vector2 (
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 
-		-1 if Input.is_action_just_pressed("ui_up") and is_on_floor() else 1
-	)
-	
-func calculate_velocity(linear_velocity: Vector2 , direction: Vector2 , speed: Vector2 , jump_interepted: bool) -> Vector2:
-	var Nvelocity = linear_velocity	
-	Nvelocity.x = speed.x * direction.x
-	Nvelocity.y += gravity * get_physics_process_delta_time()
-	if direction.y == -1:
-		Nvelocity.y = speed.y * direction.y
-	if jump_interepted:
-		Nvelocity.y = 0.0
-	return Nvelocity  
-	
+func _physics_process(delta):
+	velocity = player_movement(delta)
+	velocity.y += GRAVITY * delta # applying the gravity here 
+	velocity = move_and_slide(velocity , Vector2.UP)
+
+
+func player_movement(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")  # getting the input vector
+	input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward( input_vector * MAX_SPEED , ACCELERATION * delta) # using the "move_toward()" here to reach the max speed in the input direction and by acceleration 
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO , FRICTION * delta) # friction being applied using friction to go towards ZERO
+	if is_on_floor() and Input.is_action_pressed("ui_up"): 
+		velocity.y -= JUMP_HEIGHT 
+	if velocity.y < 0 and not is_on_floor():
+		GRAVITY = 2000
+	else:
+		GRAVITY = 700
+	return velocity
+
+
 
