b70ac876c79d373580471909076aa7330225da6e
smr.me <smr.20samar@gmail.com>
2021-06-10 00:16:09 +0530

    Updated the Player movement
    - Got rid of the accelartion of gravity on the player.
    - using lerp for acceleration rather than using move_towards().
    - Added the corner tiles of backend tileset.

--- source/scripts/Player.gd
+++ source/scripts/Player.gd
@@ -1,13 +1,12 @@
-extends KinematicBody2D
+extends KinematicBody2D 
 
-export var ACCELERATION = 500
+export var ACCELERATION = 0.9
 export var MAX_SPEED = 105
-export var FRICTION = 500
+export var FRICTION = 0.2
 export var GRAVITY = 700
 export var JUMP_HEIGHT = 500
 
 var velocity = Vector2.ZERO	
-
 
 func _physics_process(delta):
 	velocity = player_movement(delta)
@@ -19,10 +18,10 @@ func player_movement(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")  # getting the input vector
 	input_vector = input_vector.normalized()
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward( input_vector * MAX_SPEED , ACCELERATION * delta) # using the "move_toward()" here to reach the max speed in the input direction and by acceleration 
+	if input_vector.x != 0:
+		velocity.x = lerp( velocity.x , input_vector.x * MAX_SPEED , ACCELERATION * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO , FRICTION * delta) # friction being applied using friction to go towards ZERO
+		velocity.x = lerp(velocity.x , 0 , FRICTION)  
 	if is_on_floor() and Input.is_action_pressed("ui_up"): 
 		velocity.y -= JUMP_HEIGHT 
 	if velocity.y < 0 and not is_on_floor():
