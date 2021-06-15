fc1fb86b9b5ecc882c720ee7a6d4fc85b4bba7c0
smr.me <smr.20samar@gmail.com>
2021-06-16 01:05:54 +0530

    Added the function to limit the player to enter the backend world.
    - Added the PlayerStats.tscn to autoload singelton as it has to be used in multiple files.
    - Added the backend limiter bar to the world scene without any functionality (this means that the icons for the bar does not update for now)

--- source/scripts/Backend_trans.gd
+++ source/scripts/Backend_trans.gd
@@ -1,23 +1,33 @@
 extends Node 
 
-
+var stats = PlayerStats
 var texture = load("res://source/TileSets/animatedTexture.tres")
 var state: String = "frontend"
-var backendLimiter = 3
+var backendLimiter = 5
 
-func _physics_process(_delta):
-	if Input.is_action_just_pressed("ui_accept") && state == "frontend":
-		state = "backend"
-		backendLimiter -= 1
-	elif Input.is_action_just_pressed("ui_accept") && state == "backend":
+func _physics_process(_delta):	
+	stats.limiterSet(backendLimiter)
+	backendPower()
+
+
+func backendPower():
+	if backendLimiter >= 0:
+		
+		if Input.is_action_just_pressed("ui_accept") && state == "frontend":
+			state = "backend"
+			backendLimiter -= 1
+		elif Input.is_action_just_pressed("ui_accept") && state == "backend":
+			state = "frontend"
+			backendLimiter -= 1
+		if state == "backend":
+			texture.set_current_frame(1)
+		elif state == "frontend":
+			texture.set_current_frame(0)
+		
+		if Input.is_action_just_pressed("ui_accept"):
+			print(stats.limiterCheck())
+	else:
 		state = "frontend"
-		backendLimiter -= 1
-	if state == "backend":
-		texture.set_current_frame(1)
-	elif state == "frontend":
-		texture.set_current_frame(0)
-
-
 # if Input.is_action_just_pressed("gun safety") and safety = false:
 # 	safety = true
 # elif Input.is_action_just_pressed("gun safety") and safety = true:
--- source/scripts/Stats.gd
+++ source/scripts/Stats.gd
@@ -1,7 +1,23 @@
 extends Node
 
-var backend = preload ("res://source/scripts/Backend_trans.gd")
+var limiter: int = 0
 onready var energy_sprite = $Limiter
 
-func _process(_delta):
-	# print(backendLimiter)
+func limiterSet(limiterValue : int):
+	limiter = limiterValue
+	
+func limiterCheck() -> String:
+	var print_string = ""
+	if limiter == 5: 
+		print_string = ("you have used your first backend power and you are in backend")
+	if limiter == 4:
+		print_string = ("you have now returned from the backend and you have 2 backend powers left")
+	if limiter == 3:
+		print_string = ("you have used your second backend power and you are in backend")
+	if limiter == 2:
+		print_string = ("you have now returned from the backend and you have 1 backend power left")
+	if limiter == 1:
+		print_string = ("you have used your third backend power and you are in backend")
+	if limiter == 0:
+		print_string = ("you have now returned from the backend and you have 0 backend power left") 
+	return print_string
